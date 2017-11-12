4b163567bc72339c9b8cd8d57d7015bcaca292a1
Luyen Vu <vuhailuyen1991@gmail.com>
2017-11-12 16:09:44 +0100

    Update README, extract average method

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # README
 
 Simple Movie DB app built on top of Rails 5.1.4
+
+
+```ruby
+run seeds to create categories
+
+rails db:seeds
+```
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,4 +7,8 @@ class Movie < ApplicationRecord
   scope :full_text_search, ->(text) { where('movies.title LIKE ? OR movies.text LIKE ?', "%#{text}%", "%#{text}%") }
   scope :by_category, ->(category_id) { joins(:category).where(categories: { id: category_id }) }
   scope :by_rating, ->(rate) { joins(:ratings).where(ratings: { rate: rate }) }
+
+  def average_rating
+    ratings.average(:rate).to_i
+  end
 end
